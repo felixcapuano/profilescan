df3d1835f8290fc342c46fb774197c3fa52bbcfc
MammothOG <felix.capuano@gmail.com>
2022-03-11 22:45:06 +0100

    fix profiles|id in express routing

--- src/server.js
+++ src/server.js
@@ -3,9 +3,10 @@ const axios = require('axios');
 
 const app = express();
 
-const steamIdParser = async (req, res, next) => {
-  const steamLink = `https://steamcommunity.com/id/${req.params.userId}`;
-  const steamPage = await axios.get(steamLink);
+const findId = async (req, res, next) => {
+  req.steamLink = `https://steamcommunity.com/${req.originalUrl}`;
+  const steamPage = await axios.get(req.steamLink);
+
   if (steamPage.data.match(/<title>Steam Community :: Error<\/title>/)) {
     return res.status(404).send('Profile not found');
   }
@@ -16,15 +17,16 @@ const steamIdParser = async (req, res, next) => {
 
   const { steamid } = JSON.parse(match[0].groups.profileData);
 
-  req.steamid = steamid;
+  req.steamId = steamid;
   next();
 };
 
 const faceitfinderRedirect = (req, res) => {
-  res.redirect(`https://faceitfinder.com/profile/${req['steamid']}`);
+  res.redirect(`https://faceitfinder.com/profile/${req.steamId}`);
 };
 
-app.get('/id/:userId', steamIdParser, faceitfinderRedirect);
-app.get('/', (req, res) => res.send('hello world'));
+app.get('/:pi(profiles|id)/:userId', findId, faceitfinderRedirect);
+
+app.get('/', (req, res) => res.send('hello world`<br>nothing much to do here'));
 
 module.exports = app;
