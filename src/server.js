8f0c3d1b45e7821b0275ad6da30ee9ec328bdaa8
felixcapuano <felix.capuano@gmail.com>
2022-03-13 12:25:46 +0100

    logging system

--- src/server.js
+++ src/server.js
@@ -8,21 +8,32 @@ const findId = async (req, res, next) => {
   const steamPage = await axios.get(req.steamLink);
 
   if (steamPage.data.match(/<title>Steam Community :: Error<\/title>/)) {
-    return res.status(404).send('Profile not found');
+    console.error(`GET ${req.originalUrl} Steam profile not found`);
+    return res.status(404).send(`${req.params.userId} Steam profile not found`);
   }
 
   const match = [
     ...steamPage.data.matchAll(/g_rgProfileData = (?<profileData>{.*})/g),
   ];
+  if (!match) {
+    console.error(`GET ${req.originalUrl} Steam id not found in page`);
+    return res.status(500).send(`${req.params.userId} Internal error`);
+  }
 
   const { steamid } = JSON.parse(match[0].groups.profileData);
+  if (!steamid) {
+    console.error(`GET ${req.originalUrl} Steamid not found in match`);
+    return res.status(500).send(`${req.params.userId} Internal error`);
+  }
 
   req.steamId = steamid;
   next();
 };
 
 const faceitfinderRedirect = (req, res) => {
-  res.redirect(`https://faceitfinder.com/profile/${req.steamId}`);
+  const faceitfinderUrl = `https://faceitfinder.com/profile/${req.steamId}`;
+  console.log(`GET ${faceitfinderUrl}`);
+  res.redirect(faceitfinderUrl);
 };
 
 app.get('/:pi(profiles|id)/:userId', findId, faceitfinderRedirect);
