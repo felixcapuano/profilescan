62a3ab7a1369256eac9c584dc7000c5a869951c0
felixcapuano <felix.capuano@gmail.com>
2022-03-14 21:12:16 +0100

    fixing profile_id route

---
+++ src/routes/index.js
@@ -0,0 +1,7 @@
+
+const router = require('express').Router();
+
+router.use(require('./api'))
+router.use('/:pi(profiles|id)', require('./profiles_id'))
+
+module.exports = router;
--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 
-app.use('/api', require('./api'));
+app.use(require('./routes'));
 app.get('/', (req, res) => res.send('hello world`<br>nothing much to do here'));
 
 module.exports = app;
