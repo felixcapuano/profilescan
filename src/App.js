f58cb95f44c351a92c1609447d72f872f13cf624
felixcapuano <felix.capuano@gmail.com>
2022-03-15 16:11:57 +0100

    react migration

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
 function App() {
-  return <div className='App'>Hello world!</div>;
+  return (
+    <div className='App'>
+      <h1>CSGO account scanner </h1>
+      <p>Steam username : </p>
+      <p>Faceit username</p>
+      <p>Steam id : </p>
+      <p>Number of friend : </p>
+      <p>Hours Played : </p>
+      <p>Hours Played (last 2 week) : </p>
+    </div>
+  );
 }
 
 export default App;
