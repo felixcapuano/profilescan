cc629bc7a15912d35d8eabd0049cdfe5725965f3
felixcapuano <felix.capuano@gmail.com>
2022-03-15 14:13:16 +0100

    ratelimiter and slowdown

--- src/routes/api/v1/steam/getplayerachievements.js
+++ src/routes/api/v1/steam/getplayerachievements.js
@@ -1,4 +1,32 @@
 // or  GetUserStatsForGame ???
 const router = require("express").Router();
+const steamInstance = require("./steamInstance");
+
+router.get("/getplayerachievements/:steamid/", async (req, res) => {
+  try {
+    const steamResponse = await steamInstance.get(
+      "/ISteamUserStats/GetUserStatsForGame/v0002/",
+      {
+        params: {
+          appid: "730",
+          steamid: req.params.steamid,
+        },
+      }
+    );
+
+    console.log(`GET ${req.originalUrl}`);
+    return await res
+      .status(200)
+      .contentType("application/json")
+      .send(steamResponse.data);
+  } catch (error) {
+    console.error(
+      `GET ${req.originalUrl} "Steamid is invalid or/and player as no record for csgo."`
+    );
+    return await res
+      .status(404)
+      .send("The steam id is invalid or player as no record for csgo.");
+  }
+});
 
 module.exports = router;
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
 
-router.use(require("./api"));
+router.use("/api", require("./api"));
 router.use("/:pi(profiles|id)", require("./profiles_id"));
 
 module.exports = router;
