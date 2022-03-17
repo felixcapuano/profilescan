643aeb49233cf1651114685402cfb61034b27097
felixcapuano <felix.capuano@gmail.com>
2022-03-17 18:26:36 +0100

    connection redis cloud

--- server/routes/redisInstance.js
+++ server/routes/redisInstance.js
@@ -3,22 +3,25 @@ const { createClient } = require("redis");
 let connected = false;
 
 const client = createClient({
-  // url: `redis://${process.env.REDIS_URL}`,
-  socket: {
-    host: process.env.REDIS_HOST,
-    port: process.env.REDIS_PORT,
-  },
+  url: `redis://${process.env.REDIS_HOST}:${process.env.REDIS_PORT}`,
+  // socket: {
+  //   host: process.env.REDIS_HOST,
+  //   port: process.env.REDIS_PORT,
+  // },
   username: process.env.REDIS_USERNAME,
   password: process.env.REDIS_PASSWORD,
   database: process.env.REDIS_DATABASE,
 });
 
-client.connect().then(() => {
-  console.log("Redis Client Connected");
-  connected = true;
-}).catch((err) => {
-  console.error(`Fail to connect with Redis : ${err}`);
-})
+client
+  .connect()
+  .then(() => {
+    console.log("Redis Client Connected");
+    connected = true;
+  })
+  .catch((err) => {
+    console.error(`Fail to connect with Redis : ${err}`);
+  });
 
 const cache = async (req, res, next) => {
   if (!connected) return;
