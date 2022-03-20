ae011f409dcd6f43d8740451d6ddb605e7eed8ba
felixcapuano <felix.capuano@gmail.com>
2022-03-20 21:59:51 +0100

    get community profile V2

--- server/routes/api/v2/steam/getcommunityprofile.js
+++ server/routes/api/v2/steam/getcommunityprofile.js
@@ -2,37 +2,74 @@ const router = require("express").Router();
 const { parseStringPromise } = require("xml2js");
 const axios = require("axios");
 
-router.get("/getcommunityprofile", async (req, res) => {
+const getCommunityProfile = async (req, res, next) => {
+  if (req.cached) await next();
   if (!req.query.path) {
-    return res.status(400).send("'path' must be set in the request query");
+    await next({
+      error: "'path' must be set in the request query",
+    });
   }
+
   req.steamLink = `https://steamcommunity.com/${req.query.path}?xml=1`;
 
-  let steamPageXml;
   try {
-    steamPageXml = await axios.get(req.steamLink);
+    const steamPageXml = await axios.get(req.steamLink);
+    const steamPageObj = await parseStringPromise(steamPageXml.data);
+    req.data = steamPageObj.response;
   } catch (error) {
-    console.log(error);
-    console.error(`GET ${req.originalUrl} "Failed to fetch steam profile"`);
-    return res.status(404).send(`Steam profile not found`);
+    await next(error);
   }
 
-  let steamPageObj;
-  try {
-    steamPageObj = await parseStringPromise(steamPageXml.data);
-  } catch (error) {
-    console.error(`GET ${req.originalUrl} "Failed to parse xml"`);
-    return res.status(404).send(`${req.params.userId} Steam profile not found`);
-  }
+  await next();
 
-  if (steamPageObj.response?.error) {
-    console.error(
-      `GET ${req.originalUrl} "${steamPageObj.response.error.toString()}"`
-    );
-    return res.status(404).send(`${req.params.userId} Steam profile not found`);
-  }
-  console.log(`GET ${req.originalUrl}`);
-  return res.status(200).contentType("application/json").send(steamPageObj);
-});
+  // if (steamPageObj.response?.error) {
+  //   console.error(
+  //     `GET ${req.originalUrl} "${steamPageObj.response.error.toString()}"`
+  //   );
+  //   return res.status(404).send(`${req.params.userId} Steam profile not found`);
+  // }
+  // console.log(`GET ${req.originalUrl}`);
+  // return res.status(200).contentType("application/json").send(steamPageObj);
+};
+
+router.get("/getcommunityprofile", [
+  pullCache,
+  getCommunityProfile,
+  pushCache,
+  response,
+]);
+
+// router.get("/getcommunityprofile", async (req, res) => {
+//   if (!req.query.path) {
+//     return res.status(400).send("'path' must be set in the request query");
+//   }
+//   req.steamLink = `https://steamcommunity.com/${req.query.path}?xml=1`;
+
+//   let steamPageXml;
+//   try {
+//     steamPageXml = await axios.get(req.steamLink);
+//   } catch (error) {
+//     console.log(error);
+//     console.error(`GET ${req.originalUrl} "Failed to fetch steam profile"`);
+//     return res.status(404).send(`Steam profile not found`);
+//   }
+
+//   let steamPageObj;
+//   try {
+//     steamPageObj = await parseStringPromise(steamPageXml.data);
+//   } catch (error) {
+//     console.error(`GET ${req.originalUrl} "Failed to parse xml"`);
+//     return res.status(404).send(`${req.params.userId} Steam profile not found`);
+//   }
+
+//   if (steamPageObj.response?.error) {
+//     console.error(
+//       `GET ${req.originalUrl} "${steamPageObj.response.error.toString()}"`
+//     );
+//     return res.status(404).send(`${req.params.userId} Steam profile not found`);
+//   }
+//   console.log(`GET ${req.originalUrl}`);
+//   return res.status(200).contentType("application/json").send(steamPageObj);
+// });
 
 module.exports = router;
