b5bbc131d04167832810c56a15c6b217ec2cf343
felixcapuano <felix.capuano@gmail.com>
2022-03-21 10:34:36 +0100

    response status

--- server/routes/api/v2/handler/response.js
+++ server/routes/api/v2/handler/response.js
@@ -2,9 +2,16 @@ const response = async (req, res) => {
 
   console.log(`GET ${req.originalUrl}${req.cached ? " use cache" : ""}`);
 
-  await res
-    .contentType("application/json")
-    .send(req.data);
+  res.contentType("application/json");
+
+  if (req.cached) {
+    await res.status(304);
+  } else if (req.data) {
+    await res.status(302);
+  } else {
+    await res.status(404);
+  }
+  await res.send(req.data);
 }
 
 module.exports = response;
