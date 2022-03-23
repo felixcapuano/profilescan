39a368126adb76cd8bcef88730a3f33b24485a6b
felixcapuano <felix.capuano@gmail.com>
2022-03-23 14:56:02 +0100

    port deployement

--- server/entrypoint.js
+++ server/entrypoint.js
@@ -20,7 +20,7 @@ if (process.env.NODE_ENV !== "development") {
 }
 
 if (process.env.NODE_ENV !== "test") {
-  const port = process.env.REACT_APP_SERVER_PORT || 80;
+  const port = process.env.PORT || 80;
   app.listen(port, () => console.log(`listening on port ${port}`));
 }
 
