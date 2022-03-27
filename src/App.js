6126d51679f59848f8f926f337ef1dcd2bc831cb
MammothOG <felix.capuano@gmail.com>
2022-03-27 15:17:23 +0200

    Home front

--- src/App.js
+++ src/App.js
@@ -1,17 +1,40 @@
 import { Route, Routes } from "react-router-dom";
 import Profile from "./Profile";
 import Home from "./Home";
+import Navbar from 'react-bootstrap/Navbar';
+import Container from 'react-bootstrap/Container'
+import { Col, Row } from "react-bootstrap";
 
 const App = () => {
   console.log(process.env.NODE_ENV);
   return (
     <div className="App">
-      <h1>Steam community profile scanner</h1>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/profiles/:id" element={<Profile />} />
-        <Route path="/id/:id" element={<Profile />} />
-      </Routes>
+      <Navbar bg='light'>
+        <Container>
+          <Navbar.Brand href="#home">
+            <img
+              alt=""
+              src="/logo.svg"
+              width="30"
+              height="30"
+              className="d-inline-block align-top"
+            />{' '}
+            Steam Scanner
+          </Navbar.Brand>
+        </Container>
+      </Navbar>
+      <Container>
+        <Row>
+          <Col md={{ span: 8, offset: 2 }} style={{ backgroundColor: 'blue' }}>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/profiles/:id" element={<Profile />} />
+              <Route path="/id/:id" element={<Profile />} />
+            </Routes>
+          </Col>
+        </Row>
+
+      </Container>
     </div>
   );
 };
--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,13 @@
+import { Container } from "react-bootstrap";
+import './Home.css';
+
 const Home = () => {
   return (
     <div className="Home">
-      <h3>Hello world!!!</h3>
+      <div className="Title">
+        <h3 className="PrimaryTitle text-center">Hello world!!!</h3>
+        <h4 className="SecondaryTitle text-center">Secondary title</h4>
+      </div>
     </div>
   );
 };
