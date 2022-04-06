90d6ec85b3202048382181db64c7c99f2f3d531a
felixcapuano <felix.capuano@gmail.com>
2022-04-06 19:11:04 +0200

    home direct link to my profile

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div className="Home">
+      <h1>Home Page</h1>
+      <a href="/profiles/76561198069504185">My profile</a>
+    </div>
+  );
+};
+
+export default Home;
